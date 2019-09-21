321161cdb2e1c04535334f446a97568ad1ac875c
Ahmed Noor <ahmednr123@gmail.com>
2019-09-21 17:22:04 +0530

    cleaning some frontend code

--- js/views/chat.js
+++ js/views/chat.js
@@ -51,7 +51,8 @@ const ChatView = {
 			chat_id: chat_id,
 			old_msg_id: -1,
 			new_msg_id: -1,
-			more: false
+			more: false,
+			avoidScroll: false
 		}
 
 		$('chat-app').style.width = "600px";
@@ -59,97 +60,63 @@ const ChatView = {
 
 		$('#go_back').addEventListener('click', () => DashboardView.show())
 		$('#send_msg').addEventListener('click', this.sendMessage);
-		$('#refresh_btn').addEventListener('click', this.getNewMessages);
+		$('#refresh_btn').addEventListener('click', () => 
+				this.getMessages(MessageType.NEW, this.data.new_msg_id)
+		);
 		
-		this.getMessages();
+		this.getMessages(MessageType.CURRENT);
 		this.render();
 	},
 
-	getMessages: function () {
-		$xhrRequest(
-			`/ChatApp/messages?type=${MessageType.CURRENT}&chat_id=${ChatView.data.chat_id}`,
+	getMessages: function (type, msg_id) {
+		let api = `/ChatApp/messages
+				?type=${type}
+				&chat_id=${ChatView.data.chat_id}`
+
+		if (msg_id) 
+			api += `&msg_id=${msg_id}`
+		
+		$xhrRequest(api,
 			(res) => {
 				console.log("GET[/messages] : " + res);
 				res = res.trim();
 				res = JSON.parse(res);
 				console.log("Cleaning JSON");
 
-				ChatView.data.chatMessages = []
-				if (res[0])
-					ChatView.data.new_msg_id = res[0].msg_id;
+				if (type == MessageType.CURRENT) {
+					ChatView.data.chatMessages = []
+					if (res[0])
+						ChatView.data.new_msg_id = res[0].msg_id;
+				}
 
-				if (res.length >= 20)
-					ChatView.data.more = true;
+				if (type == MessageType.CURRENT || type == MessageType.OLD) {
+					if (res.length >= 20)
+						ChatView.data.more = true;
+					else
+						ChatView.data.more = false;
+				}
+
+				for (let json of res) {
+					let chatMessage = {msg_id:json.msg_id, msg:json.message, time:json.time}
+
+					if (ChatView.data.receiver == json.sender)
+						chatMessage["receiver"] = true;
+
+					if (type == MessageType.CURRENT || type == MessageType.OLD) {
+						ChatView.data.chatMessages.unshift(chatMessage);
+						ChatView.data.old_msg_id = json.msg_id;
+					} else {
+						ChatView.data.chatMessages.push(chatMessage);
+						ChatView.data.new_msg_id = json.msg_id;
+					}
+					
+				}
+
+				if (type == MessageType.OLD)
+					ChatView.data.scroll = false;
 				else
-					ChatView.data.more = false;
+					ChatView.data.scroll = true;
 
-				for (let json of res) {
-					let chatMessage = {msg_id:json.msg_id, msg:json.message, time:json.time}
-
-					if (ChatView.data.receiver == json.sender)
-						chatMessage["receiver"] = true;
-
-					ChatView.data.chatMessages.unshift(chatMessage);
-					ChatView.data.old_msg_id = json.msg_id;
-				}
-				ChatView.render();
-				$('chat-messages').scrollTop = $('chat-messages').scrollHeight;
-			}
-		)
-	},
-
-	getOldMessages: function () {
-		$xhrRequest(
-			`/ChatApp/messages
-				?type=${MessageType.OLD}
-				&chat_id=${ChatView.data.chat_id}
-				&msg_id=${ChatView.data.old_msg_id}`,
-			(res) => {
-				console.log("GET[/messages] : " + res);
-				res = res.trim();
-				res = JSON.parse(res);
-				console.log("Cleaning JSON");
-
-				if (res.length >= 20)
-					ChatView.data.more = true;
-				else
-					ChatView.data.more = false;
-
-				for (let json of res) {
-					let chatMessage = {msg_id:json.msg_id, msg:json.message, time:json.time}
-
-					if (ChatView.data.receiver == json.sender)
-						chatMessage["receiver"] = true;
-
-					ChatView.data.chatMessages.unshift(chatMessage);
-					ChatView.data.old_msg_id = json.msg_id;
-				}
-				ChatView.render(false);
-			}
-		)
-	},
-
-	getNewMessages: function () {
-		$xhrRequest(
-			`/ChatApp/messages
-				?type=${MessageType.NEW}
-				&chat_id=${ChatView.data.chat_id}
-				&msg_id=${ChatView.data.new_msg_id}`,
-			(res) => {
-				console.log("GET[/messages] : " + res);
-				res = res.trim();
-				res = JSON.parse(res);
-				console.log("Cleaning JSON");
-
-				for (let json of res) {
-					let chatMessage = {msg_id:json.msg_id, msg:json.message, time:json.time}
-
-					if (ChatView.data.receiver == json.sender)
-						chatMessage["receiver"] = true;
-
-					ChatView.data.chatMessages.push(chatMessage);
-					ChatView.data.new_msg_id = json.msg_id;
-				}
 				ChatView.render();
 			}
 		)
@@ -175,18 +142,20 @@ const ChatView = {
 		)
 	},
 
-	render: function (avoidScroll) {
+	render: function () {
 		$('chat-messages').innerHTML = "";
 
 		if(this.data.more) {
 			$('chat-messages').innerHTML = this.html.show_more;
-			$('show-more').addEventListener('click', this.getOldMessages);
+			$('show-more').addEventListener('click', 
+				this.getMessages(MessageType.OLD, this.data.old_msg_id)
+			);
 		}
 		
 		for (let message of this.data.chatMessages)
 			$('chat-messages').innerHTML += this.html.message(message);
 		
-		if (avoidScroll !== false)
+		if (this.data.scroll === true)
 			$('chat-messages').scrollTop = $('chat-messages').scrollHeight;
 	}
 }
